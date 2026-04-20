2439d5b773b3b01ea9dd88c8d9863c340bc0f0f4
Chenxin Hu <1045321156@qq.com>
2026-04-20 14:01:53 +0800

    Google Ad implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <GoogleTagManager gtmId="AW-18105614952" />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
