4382aa417f265b1f5691c327eeecf0a8e30a5953
Chenxin Hu <1045321156@qq.com>
2026-04-20 13:09:01 +0800

    Stable API calls

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -141,7 +141,7 @@ export default function BookingPage() {
     };
 
     try {
-      const response = await fetch('https://scutbl.top:3001/booking', {
+      const response = await fetch('https://www.scutbl.top:3001/booking', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
